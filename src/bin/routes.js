a9e7210111d8ffb2d9eed7a2e7795841ec2f0073
Pertinate <nicholas@pertinate.info>
2020-03-28 22:04:17 -0500

    serving index html

--- src/bin/routes.js
+++ src/bin/routes.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import V1 from './controllers/V1';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const v1 = new V1();
 const router = express.Router();
 
 router.use(cookieParser());
 
+router.get('/', (request, response) =>
+    response.sendFile(path.resolve('../PortfolioFrontend/build/index'))
+);
 router.get('/test', (request, response) => response.send({ message: 'test' }));
 
 // V1 routes go here
